3f263acaec06e34c05c6afa8d284d7487e96eba5
Tristan Denmat <tristan.denmat@accenture.com>
2021-01-04 16:24:47 +0100

    add simple webserver

---
+++ back-app/src/test/java/fr/liksi/formation/keycloak/backapp/BackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.liksi.formation.keycloak.backapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
