08eeaeb684dee02884362b54ce144f1a1d9fc375
DENMAT Tristan <Tristan.denmat@liksi.fr>
2023-06-18 15:21:39 +0200

    fix logout configuration

--- secret-webapp/src/main/java/fr/liksi/formation/keycloak/backapp/config/KeycloakConfig.java
+++ secret-webapp/src/main/java/fr/liksi/formation/keycloak/backapp/config/KeycloakConfig.java
@@ -5,21 +5,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @ConditionalOnProperty(value = "authentication-mode", havingValue = "keycloak")
 public class KeycloakConfig {
 
-	   @Bean
-	   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-			  http.authorizeHttpRequests(auth ->
-							  auth.requestMatchers("/secret/**").authenticated().anyRequest().permitAll()
-					  )
-					  .oauth2Login(Customizer.withDefaults());
-			  return http.build();
-	   }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/secret/**").authenticated().anyRequest().permitAll())
+                .oauth2Login(Customizer.withDefaults()).logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)));
+        return http.build();
+    }
 
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler(ClientRegistrationRepository clientRegistrationRepository) {
+        OidcClientInitiatedLogoutSuccessHandler successHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri("/");
+        return successHandler;
+    }
 
 }
 
